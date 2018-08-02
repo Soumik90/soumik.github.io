0ff054d9edb37f1c95382b994cf96b42645612d6
Soumik90 <42038718+Soumik90@users.noreply.github.com>
2018-08-02 20:24:25 +0530

    first website upload

---
+++ MvcApplication2/Models/OrderDataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication2.Models
+{
+    public class OrderDataContext:DbContext
+    {
+        public DbSet<Order> Order { get; set; }
+        public OrderDataContext(): base("DefaultConnection")
+        {
+
+        }
+    }
+}
